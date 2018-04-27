eceb0a4adfdbdaa4ea3c73ef0cb0ad9e7e0b9ecf
Anthony <github email address>
2018-04-27 12:00:06 -0400

    updated reservation model for notifications

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,15 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :parking
+
+  after_create_commit :create_notification
+
+  private
+
+  def created_notifcation
+    type = self.room.Instant? ? "New Booking" : "New Request"
+    guest = User.find(self.user_id)
+
+    Notification.create(content: "#{type} from #{guest.fullname}", user_id: self.room.user_id)
+  end
 end
