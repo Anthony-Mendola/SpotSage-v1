af2c7b15a11d65576ef36e3cf283322b357b5390
Anthony <github email address>
2018-05-06 15:07:56 -0400

    due to heroku actioncable issues, switched back from postgres to sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -10,32 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180427154907) do
+ActiveRecord::Schema.define(version: 20180421044024) do
 
-  create_table "conversations", force: :cascade do |t|
-    t.integer "sender_id"
-    t.integer "recipient_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "messages", force: :cascade do |t|
-    t.text "context"
-    t.integer "user_id"
-    t.integer "conversation_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
-    t.index ["user_id"], name: "index_messages_on_user_id"
-  end
-
-  create_table "notifications", force: :cascade do |t|
-    t.string "content"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_notifications_on_user_id"
-  end
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "parkings", force: :cascade do |t|
     t.string "space_type"
@@ -51,7 +29,7 @@ ActiveRecord::Schema.define(version: 20180427154907) do
     t.boolean "is_heating"
     t.integer "price"
     t.boolean "active"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.float "latitude"
@@ -60,7 +38,7 @@ ActiveRecord::Schema.define(version: 20180427154907) do
   end
 
   create_table "photos", force: :cascade do |t|
-    t.integer "parking_id"
+    t.bigint "parking_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_file_name"
@@ -71,8 +49,8 @@ ActiveRecord::Schema.define(version: 20180427154907) do
   end
 
   create_table "reservations", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "parking_id"
+    t.bigint "user_id"
+    t.bigint "parking_id"
     t.datetime "start_at"
     t.datetime "end_at"
     t.integer "price"
@@ -81,22 +59,6 @@ ActiveRecord::Schema.define(version: 20180427154907) do
     t.datetime "updated_at", null: false
     t.index ["parking_id"], name: "index_reservations_on_parking_id"
     t.index ["user_id"], name: "index_reservations_on_user_id"
-  end
-
-  create_table "reviews", force: :cascade do |t|
-    t.text "comment"
-    t.integer "star", default: 1
-    t.integer "parking_id"
-    t.integer "reservation_id"
-    t.integer "guest_id"
-    t.integer "host_id"
-    t.string "type"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["guest_id"], name: "index_reviews_on_guest_id"
-    t.index ["host_id"], name: "index_reviews_on_host_id"
-    t.index ["parking_id"], name: "index_reviews_on_parking_id"
-    t.index ["reservation_id"], name: "index_reviews_on_reservation_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -121,10 +83,13 @@ ActiveRecord::Schema.define(version: 20180427154907) do
     t.string "image"
     t.string "phone_number"
     t.text "description"
-    t.integer "unread", default: 0
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "parkings", "users"
+  add_foreign_key "photos", "parkings"
+  add_foreign_key "reservations", "parkings"
+  add_foreign_key "reservations", "users"
 end
